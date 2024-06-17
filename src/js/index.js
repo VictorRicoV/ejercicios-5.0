a6ebd7b22da41b5f85ed605dc87a9f19a8367e89
Andrea <lessricc@gmail.com>
2024-06-17 21:56:35 +0200

    aprendiendo

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
 
 function suma(a, b) {
   return a + b;
@@ -9,7 +9,7 @@ suma(2, 2);
 function saludar(nombre) {
   return `¡Hola, ${nombre}`;
 }
-saludar('Carlos');
+saludar("Carlos");
 
 function multiplicar(a, b) {
   return a * b;
@@ -40,16 +40,32 @@ function cuadrado(numero) {
 // alert(cuadrado(3));
 
 function concatenarCadenas(...cadenas) {
-  return cadenas.join(' ');
+  return cadenas.join(" ");
 }
 
-console.log(concatenarCadenas('Hola', 'mundo,', 'chao', '!')); // Salida: "Hola mundo !"
+console.log(concatenarCadenas("Hola", "mundo,", "chao", "!")); // Salida: "Hola mundo !"
 
 //Generador de Saludos Personalizados Crea una función crearSaludo que acepte un saludo inicial como argumento y devuelva (return) una función que acepte un nombre como argumento y devuelva una cadena que combine el saludo inicial con el nombre.
 
 function crearSaludo(saludo) {
-  return function nombre(name) {
-    saludo.join(' ');
+  return function (nombre) {
+    return `${saludo}, ${nombre}!`;
   };
 }
+<<<<<<< HEAD
 console.log(crearSaludo('Hola, que tal'));
+
+//Generadora de Números Pares Crea una función generadora que produzca números pares de 2 en 2.
+
+function* numerosPares() {
+  for (let i = 2; i >= 2; i += 2) {
+    yield i;
+  }
+}
+const generador = numerosPares();
+console.log(generador.next());
+=======
+const saludoPersonalizado = crearSaludo("Hola");
+
+console.log(saludoPersonalizado("Manuel"));
+>>>>>>> 4029ca8eed08b2032c908c78a81757e0f7039677
